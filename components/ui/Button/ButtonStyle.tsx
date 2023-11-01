187ccf2759a57cf44ba5ac4e4c62117d542cabf2
pandyama <pandyama2@gmail.com>
2023-11-01 08:00:51 -0600

    button component styling changes

--- components/ui/Button/ButtonStyle.tsx
+++ components/ui/Button/ButtonStyle.tsx
@@ -2,18 +2,22 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   button: {
-    display: "flex",
+    // display: "flex",
+    position: "absolute",
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 60,
-    width: 60,
-    height: 60,
+    width: 55,
+    height: 55,
     elevation: 10,
     backgroundColor: "black",
+    // keep FAB in bottom right using the below properties
+    right: 20,
+    bottom: 20,
   },
   text: {
-    fontSize: 40,
-    // lineHeight: 40,
+    position: "relative",
+    fontSize: 30,
     // fontWeight: "bold",
     // letterSpacing: 0.25,
     color: "white",
