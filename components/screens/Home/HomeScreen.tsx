90f581a08a7da1967a0ed158fba408d3f072fc10
pandyama <pandyama2@gmail.com>
2023-11-05 17:23:51 -0700

    displaying notes from asyncStorage

--- components/screens/Home/HomeScreen.tsx
+++ components/screens/Home/HomeScreen.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useIsFocused } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as SplashScreen from "expo-splash-screen";
 
 import { useFonts } from "expo-font";
@@ -13,58 +14,111 @@ import TextBox from "../../ui/TextBox/TextBox";
 import Button from "../../ui/Button/Button";
 import { Pressable } from "react-native";
 
-const Card = ({ id, name }: any) => {
+const Card = ({ id, name, color }: any) => {
   return (
-    <View style={styles.card}>
-      <Text style={{ fontFamily: "DaiBannaSIL-Regular", fontSize: 27 }}>
+    <View style={{ ...styles.card, backgroundColor: color }}>
+      <Text
+        style={{
+          fontFamily: "DaiBannaSIL-Regular",
+          fontSize: 27,
+        }}
+      >
         {name}
       </Text>
     </View>
   );
 };
 
+// const loadNotes = async () => {
+//   // load all saved notes from AsyncStorage
+//   try {
+//     const keys = await AsyncStorage.getAllKeys();
+//     const savedNotes = await AsyncStorage.multiGet(keys);
+//     const notes = savedNotes.map((note) => JSON.parse(note[1]));
+//     // setNotes(notes);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
 export default function HomeScreen({ navigation }: any) {
+  const isFocused = useIsFocused();
+
+  const [allKeys, setKeys] = useState([]);
+
+  const [notes, setNotes] = useState<any[]>([]);
+
+  useEffect(() => {
+    // AsyncStorage.getAllKeys()
+    //   .then((res) => {
+    //     // res is an array like ["1234", "5678"]
+    //     setKeys;
+    //   })
+    //   .catch((e) => {
+    //     console.log(e);
+    //   });
+    if (isFocused) {
+      getNotes();
+    }
+  }, [isFocused]);
+
+  const getNotes = async () => {
+    try {
+      const keys = await AsyncStorage.getAllKeys();
+      console.log("🚀 ~ file: HomeScreen.tsx:63 ~ getNotes ~ keys:", keys);
+      console.log("----------------");
+      const notes = await AsyncStorage.multiGet(keys);
+      console.log("🚀 ~ file: HomeScreen.tsx:60 ~ getNotes ~ notes:", notes);
+      console.log("----------------");
+      let parsedNotes = notes.map((note) => JSON.parse(note[1] || ""));
+      console.log("returning getNotes", parsedNotes);
+
+      setNotes(parsedNotes);
+
+      return parsedNotes;
+    } catch (e) {
+      console.log("error retrieving  all notes", e);
+      // setIsLoading(false);
+    }
+  };
+
   return (
-    <ScrollView
-      contentContainerStyle={{
-        flexDirection: "row",
-        flexWrap: "wrap",
-        backgroundColor: "#252525",
-      }}
-    >
-      {Array.from(Array(10).keys()).map((item, index) => {
-        return (
-          <View style={{ width: "50%", flexDirection: "row" }}>
-            <Card name="Meet" />
-            {/* <TextBox /> */}
-          </View>
-        );
-      })}
-      {/* <View style={styles.twoColumns}>
-        <View style={styles.leftColumn}>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
+    <View style={styles.container}>
+      <ScrollView
+        contentContainerStyle={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+          backgroundColor: "#252525",
+        }}
+      >
+        {/* {Array.from(Array(10).keys()).map((item, idx) => {
+      return (
+        <View key={idx} style={{ width: "50%", flexDirection: "row" }}>
+          <Card name="Meet" />
         </View>
-        <View style={styles.rightColumn}>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
-          <TextBox></TextBox>
-        </View>
-      </View> */}
+      );
+    })} */}
+
+        {notes.map((item, idx) => {
+          return (
+            <View
+              key={idx}
+              style={{
+                width: "50%",
+                flexDirection: "row",
+              }}
+            >
+              <Card name={item.noteContent} color={item.noteColor} />
+            </View>
+          );
+        })}
+      </ScrollView>
       <Pressable
         style={styles.button}
         onPress={() => navigation.navigate("Notes")}
       >
         <Text style={styles.text}>+</Text>
       </Pressable>
-    </ScrollView>
+    </View>
   );
 }
